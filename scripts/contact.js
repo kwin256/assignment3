518bec7a36ab64611f218b212e2b2b917e0d6195
kwin256 <143911888+kwin256@users.noreply.github.com>
2023-12-09 20:53:36 -0700

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,8 +3,10 @@
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
 const submitButton = document.getElementById("submit-button");
-submitButton.addEventListener("click", ev => {
+submitButton.addEventListener("click", submit);
+
+function submit() {
     const contactPage = document.getElementById("contact-page");
     contactPage.innerHTML = "<p>Thank you for your message</p>";
     contactPage.classList.add("large-text");
-});
+}
